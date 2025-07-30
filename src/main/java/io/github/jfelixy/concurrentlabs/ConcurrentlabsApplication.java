4017f6a9c7a6fba33149285cdd87e474819363c1
jovibfel@gmail.com <jovibfel@gmail.com>
2025-07-30 19:55:33 -0300

    First commit

---
+++ src/main/java/io/github/jfelixy/concurrentlabs/ConcurrentlabsApplication.java
@@ -0,0 +1,13 @@
+package io.github.jfelixy.concurrentlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentlabsApplication.class, args);
+	}
+
+}
