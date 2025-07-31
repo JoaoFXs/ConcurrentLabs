1218e857c4d14b8bcf675d284816ba96b953097f
jovibfel@gmail.com <jovibfel@gmail.com>
2025-07-31 19:18:59 -0300

    Criação repository ProfessorRepository.java

---
+++ src/main/java/io/github/jfelixy/concurrentlabs/repository/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package io.github.jfelixy.concurrentlabs.repository;
+
+
+import io.github.jfelixy.concurrentlabs.domain.model.Laboratorio;
+import io.github.jfelixy.concurrentlabs.domain.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+}
