bf90eaeabe654f2975e1955c2a3446069b7a126d
jovibfel@gmail.com <jovibfel@gmail.com>
2025-08-05 19:27:03 -0300

    Atualização ProfessorRequest dto

--- src/main/java/io/github/jfelixy/concurrentlabs/dto/request/ProfessorRequest.java
+++ src/main/java/io/github/jfelixy/concurrentlabs/dto/request/ProfessorRequest.java
@@ -1,4 +1,8 @@
 package io.github.jfelixy.concurrentlabs.dto.request;
 
-public record ProfessorRequest() {
+public record ProfessorRequest(
+        String nome,
+        String email,
+        String matricula
+) {
 }
