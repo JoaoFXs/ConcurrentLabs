8105d7a613f776e0e0eb276261fa78782d595bee
jovibfel@gmail.com <jovibfel@gmail.com>
2025-08-05 18:27:14 -0300

    Criação ProcessamentoLoteService.java

---
+++ src/main/java/io/github/jfelixy/concurrentlabs/exceptions/FalhaProcessamentoLoteException.java
@@ -0,0 +1,7 @@
+package io.github.jfelixy.concurrentlabs.exceptions;
+
+public class FalhaProcessamentoLoteException extends RuntimeException {
+    public FalhaProcessamentoLoteException(String message) {
+        super(message);
+    }
+}
