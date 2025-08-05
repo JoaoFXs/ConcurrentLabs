c74ac9beae48fa883a2ebb184820927d60d86a64
jovibfel@gmail.com <jovibfel@gmail.com>
2025-08-05 18:27:06 -0300

    Criação ProcessamentoLoteService.java

---
+++ src/main/java/io/github/jfelixy/concurrentlabs/service/ProcessamentoLoteService.java
@@ -0,0 +1,39 @@
+package io.github.jfelixy.concurrentlabs.service;
+
+
+import io.github.jfelixy.concurrentlabs.domain.model.Reserva;
+import io.github.jfelixy.concurrentlabs.exceptions.FalhaProcessamentoLoteException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CyclicBarrier;
+
+@Service
+public class ProcessamentoLoteService {
+
+    private CyclicBarrier barrier;
+    private  List<Reserva> reservasPendentes = new CopyOnWriteArrayList<>();
+
+    public ProcessamentoLoteService(CyclicBarrier barrier) {
+        this.barrier = new CyclicBarrier(5,this::processarLote);
+    }
+
+    private void adicionarReservaLote(Reserva reserva){
+        reservasPendentes.add(reserva);
+        try {
+            barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            Thread.currentThread().interrupt();
+            throw new FalhaProcessamentoLoteException("Falha no processamento do lot");
+        }
+    }
+
+    private void processarLote(){
+        reservasPendentes.forEach(reserva -> {
+
+        });
+        reservasPendentes.clear();
+    }
+}
