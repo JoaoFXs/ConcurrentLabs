6c092d507b691bc6ac6aa3c86ce4c899d0207c17
jovibfel@gmail.com <jovibfel@gmail.com>
2025-08-06 19:18:53 -0300

    Atualização ProfessorRepository.java

--- src/main/java/io/github/jfelixy/concurrentlabs/repository/ProfessorRepository.java
+++ src/main/java/io/github/jfelixy/concurrentlabs/repository/ProfessorRepository.java
@@ -1,10 +1,9 @@
 package io.github.jfelixy.concurrentlabs.repository;
 
 
-import io.github.jfelixy.concurrentlabs.domain.model.Laboratorio;
 import io.github.jfelixy.concurrentlabs.domain.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-
+         Professor findByMatricula(String matricula);
 }
