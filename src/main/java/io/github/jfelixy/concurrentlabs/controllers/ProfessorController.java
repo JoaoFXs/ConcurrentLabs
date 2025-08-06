43ce71f86f3abe31b8269a3fb260c03d8a558dd2
jovibfel@gmail.com <jovibfel@gmail.com>
2025-08-06 19:18:44 -0300

    Atualização ProfessorController

--- src/main/java/io/github/jfelixy/concurrentlabs/controllers/ProfessorController.java
+++ src/main/java/io/github/jfelixy/concurrentlabs/controllers/ProfessorController.java
@@ -1,21 +1,38 @@
 package io.github.jfelixy.concurrentlabs.controllers;
 
 
+import io.github.jfelixy.concurrentlabs.domain.model.Professor;
 import io.github.jfelixy.concurrentlabs.dto.request.ProfessorRequest;
+import io.github.jfelixy.concurrentlabs.dto.request.response.ProfessorResponse;
+import io.github.jfelixy.concurrentlabs.repository.ProfessorRepository;
+import io.github.jfelixy.concurrentlabs.service.ProfessorService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping
+@RequestMapping("/professor")
 public class ProfessorController {
 
-    @PostMapping
-    public ResponseEntity criarProfessor(@RequestBody @Valid ProfessorRequest professor){
+    @Autowired
+    private ProfessorService professorService;
 
-        return null;
+    @PostMapping
+    public ResponseEntity criarProfessor(@RequestBody @Valid ProfessorRequest professorDTO){
+
+        Professor profSafe = professorService.criarProfessor(professorDTO);
+
+        return ResponseEntity.created(URI.create("/professor")).body(toResponse(profSafe));
+    }
+
+    private ProfessorResponse toResponse(Professor profSafe){
+        return new ProfessorResponse(profSafe.getNome(), profSafe.getEmail(), profSafe.getMatricula(), "CADASTRADO");
     }
 }
