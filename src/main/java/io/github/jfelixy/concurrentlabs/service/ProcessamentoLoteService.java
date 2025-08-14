e48143e33d120c2c9b441694d6e37bc5381cdfb9
jovibfel@gmail.com <jovibfel@gmail.com>
2025-08-14 16:19:40 -0300

    Criação de timeout para tolerancia em lotes incompletos

--- src/main/java/io/github/jfelixy/concurrentlabs/service/ProcessamentoLoteService.java
+++ src/main/java/io/github/jfelixy/concurrentlabs/service/ProcessamentoLoteService.java
@@ -5,23 +5,27 @@ import io.github.jfelixy.concurrentlabs.domain.model.Reserva;
 import io.github.jfelixy.concurrentlabs.domain.model.enums.StatusReserva;
 import io.github.jfelixy.concurrentlabs.exceptions.FalhaProcessamentoLoteException;
 import io.github.jfelixy.concurrentlabs.repository.ReservaRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.*;
 
 @Service
 @NoArgsConstructor
 public class ProcessamentoLoteService {
 
+    @Value("${app.timeout-lote-segundos:30}") // default 30s
+    private int timeoutSegundos;
 
-    private  CyclicBarrier barrier;//Barreira Ciclica que sincronizará as threads
+    private CyclicBarrier barrier;//Barreira Ciclica que sincronizará as threads
     private  List<Reserva> reservasPendentes = new CopyOnWriteArrayList<>();//Lista thread-safe para armazenar reservas pendentes
+    @Autowired
+    private NotificacaoService notificacaoService;
 
     @Autowired
     private ReservaRepository reservaRepository;//Repositório de reserva
@@ -31,17 +35,21 @@ public class ProcessamentoLoteService {
      *  Quando 5 threads chamam await(), o método processarLote() é disparado automaticamente
      * **/
 
-    public ProcessamentoLoteService(CyclicBarrier barrier) {
-        this.barrier = new CyclicBarrier(5,this::processarLote);
+    @PostConstruct // Inicializa após injeção de dependências
+    public void init() {
+        this.barrier = new CyclicBarrier(5, this::processarLote);
     }
 
     public  void adicionarReservaLote(Reserva reserva){
         reservasPendentes.add(reserva);
         try {
-            barrier.await();//Aguarda até a barreira ciclica romper em 5
+            barrier.await(timeoutSegundos, TimeUnit.SECONDS);//Aguarda até a barreira ciclica romper em 5, se demorar mais que 30 segundos para romper, envia as requisições que ja foram feitas
         } catch (InterruptedException | BrokenBarrierException e) {
             Thread.currentThread().interrupt();
             throw new FalhaProcessamentoLoteException("Falha no processamento do lot");
+        } catch (TimeoutException e) {
+            // Timeout: processa o lote atual mesmo incompleto
+            processarLote();
         }
     }
 
@@ -51,7 +59,7 @@ public class ProcessamentoLoteService {
             reserva.setStatus(StatusReserva.CONFIRMADA);
             reservaRepository.save(reserva);
             /** Sistema de notificação, será implementado em breve**/
-            //notificacaoService.enviarConfirmacao(reserva);
+            notificacaoService.enviarConfirmacao(reserva);
         });
         reservasPendentes.clear();
     }
